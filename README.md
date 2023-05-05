7ddfcd231e875d44774c484de9c2dda3a33c156b
Jaemin Kim <geminik23@gmail.com>
2023-05-05 13:36:13 +0900

    Wrapped the message and response

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Async Task Executor
 
-This project is currently in active development and is not yet ready for production use.
+This project is currently in **active development** and is not yet ready for production use.
 
 A lightweight Rust library for building extensible asynchronous task executors, designed for handling large-scale multitasking in server environments. 
 
@@ -8,9 +8,18 @@ A lightweight Rust library for building extensible asynchronous task executors, 
 
 # TODO
 - [x] Trait SharedData to access the sharable data in different thread.
-- [ ] 
-
+- [x] TaskHandler : takes a message(String type) and return the result (String) - ?use DeserializeOwned?
+- [ ] Implement the TaskMessage and Response
 - [ ] Implement the TaskExecutor with TaskHandler
+
+
+## Future work
+
+- Add a communication layers to allow clients to send task and receive responses : RESTFul API, a gRPC, a WebSocket server
+- Implement a communication layer: Develop a communication layer to allow clients to send task messages to the server and receive responses. 
+	- This could include creating a RESTful API, a gRPC service, or a WebSocket server to facilitate communication between clients and the multitask executor.
+- Improve the error handling and logging.
+- Enhance the 'authorize' method.
 
 
 
--- src/task_executor.rs
+++ src/task_executor.rs
@@ -2,7 +2,22 @@ use async_trait::async_trait;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 
+use serde::{Deserialize, Serialize};
+
 pub trait SharedData: Send + Sync + 'static {}
+
+#[derive(Serialize, Deserialize)]
+pub struct TaskMessage {
+    pub task_name: String,
+    pub payload: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Response {
+    pub success: bool,
+    pub result: Option<String>,
+    pub error: Option<String>,
+}
 
 // definition of the interface for handling tasks.
 #[async_trait]
@@ -10,5 +25,25 @@ pub trait TaskHandler<S: SharedData>: Send + Sync {
     async fn handle(&self, task_json: String, shared_data: Arc<S>) -> Result<String, String>;
     async fn authorize(&self, _shared_data: Arc<S>) -> bool {
         true
+    }
+}
+
+// The TaskExecutor struct manages a collection of TaskHandler implementations.
+pub struct TaskExecutor<S: SharedData> {
+    task_handlers: Arc<Mutex<HashMap<String, Arc<dyn TaskHandler<S>>>>>,
+    shared_data: Arc<S>,
+}
+
+impl<S: SharedData> TaskExecutor<S> {
+    pub fn new(shared_data: Arc<S>) -> Self {
+        TaskExecutor {
+            task_handlers: Arc::new(Mutex::new(HashMap::new())),
+            shared_data,
+        }
+    }
+    pub async fn register(&self, task_name: String, handler: Arc<dyn TaskHandler<S>>) {}
+
+    pub async fn execute(&self, task_name: &str, task_json: String) -> Result<String, String> {
+        unimplemented!("")
     }
 }
